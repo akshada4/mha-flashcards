1cdbf183803621d33b90475b5ef1d7e309a38319
akshada <akshadasipsap@gmail.com>
2021-06-24 00:36:58 +0530

    addition of font file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './font/futura.otf';
 
 ReactDOM.render(
   <React.StrictMode>
